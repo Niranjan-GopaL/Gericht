88df225a98efa7236c92edcd76896bd09abdd322
h <niranjan.gopal@iiitb.ac.in>
2023-02-06 06:02:36 +0530

    Adding App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>HEllo World, I figured it out what to do and how to do and where it'll get me</h1>
+    </div>
+  )
+}
+
+export default App
